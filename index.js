2595f871aadee8bd617b4f1500cece4f0798cb37
Ankit <98114752+Ankit64604@users.noreply.github.com>
2024-07-23 15:35:33 +0000

    Final commit

---
+++ index.js
@@ -0,0 +1,72 @@
+
+
+const apikey = '62947cca884bcd553a7b71608887014d' ;
+
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=' ;
+
+const searchBox= document.querySelector(".search input");
+const searchBtn= document.querySelector(".search button");
+
+const weatherIcon = document.querySelector(".weather-icon") ;
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+
+    if(response.status === 404){
+        document.querySelector(".error").style.display = "block" ;
+        document.querySelector(".weather").style.display = "none" ;
+    }
+    else {
+        var data = await response.json() ;
+
+    //  console.log(data); get the required data from here; 
+
+    document.querySelector(".city").innerHTML = data.name ;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C" ;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%" ;
+    document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
+
+    if(data.weather[0].main =="Clouds"){
+        weatherIcon.src = "images/clouds.png" ;
+    }
+    else if(data.weather[0].main =="Clear"){
+        weatherIcon.src = "images/clear.png" ;
+    }
+    else if(data.weather[0].main =="Rain"){
+        weatherIcon.src = "images/rain.png" ;
+    }
+    else if(data.weather[0].main =="Drizzle"){
+        weatherIcon.src = "images/drizzle.png" ;
+    }
+    else if(data.weather[0].main =="Mist"){
+        weatherIcon.src = "images/mist.png" ;
+    }
+
+
+    document.querySelector(".error").style.display = "none" ;
+    document.querySelector(".weather").style.display = "block" ;
+    }
+    
+    
+     
+}
+
+
+
+searchBtn.addEventListener("click" , ()=>{
+    checkWeather(searchBox.value);
+    document.querySelector(".weather").style.display = "block" ;
+     
+})
+
+searchBox.addEventListener("keypress", (event)=> {
+    
+    if (event.key == "Enter") {
+        checkWeather(searchBox.value);
+        document.querySelector(".weather").style.display = "block" ;
+       
+       
+    }
+  });
+
+
